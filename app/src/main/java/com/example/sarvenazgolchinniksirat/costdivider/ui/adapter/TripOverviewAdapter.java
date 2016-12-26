59b4ce8379895d3484d5ced1af76990cf0f7d8f9
Sarvi <sarvenaz.golchinniksirat@7p-group.com>
2016-12-26 20:15:55 +0100

    added button style and FAB

---
+++ app/src/main/java/com/example/sarvenazgolchinniksirat/costdivider/ui/adapter/TripOverviewAdapter.java
@@ -0,0 +1,22 @@
+package com.example.sarvenazgolchinniksirat.costdivider.ui.adapter;
+
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class TripOverviewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
--- app/src/main/java/com/example/sarvenazgolchinniksirat/costdivider/ui/fragment/TripsOverviewFragment.java
+++ app/src/main/java/com/example/sarvenazgolchinniksirat/costdivider/ui/fragment/TripsOverviewFragment.java
@@ -5,17 +5,25 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.sarvenazgolchinniksirat.costdivider.R;
-import com.example.sarvenazgolchinniksirat.costdivider.util.FragmentNavigationUtil;
+import com.example.sarvenazgolchinniksirat.costdivider.ui.adapter.RecyclerItemClickListener;
+import com.example.sarvenazgolchinniksirat.costdivider.ui.adapter.TripOverviewAdapter;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
-public class TripsOverviewFragment extends Fragment{
+public class TripsOverviewFragment extends Fragment implements RecyclerItemClickListener.OnItemClickListener{
+
+    @Bind(R.id.trip_list)
+    protected RecyclerView tripsRecyclerView;
+
+    private TripOverviewAdapter mAdapter;
 
     @NonNull
     public static TripsOverviewFragment newInstance(){
@@ -37,9 +45,42 @@ public class TripsOverviewFragment extends Fragment{
         return rootView;
     }
 
-    @OnClick(R.id.add_button)
+    /*@OnClick(R.id.add_button)
     public void goToTripDetail(){
         FragmentNavigationUtil.showFragment(TripDetailFragment.newInstance(), getActivity().getSupportFragmentManager(), R.id.content_pane);
 
+    }*/
+
+    /**
+     * Initializes the recyclerview.
+     */
+    private void initRecyclerView() {
+        /*tripsRecyclerView.addOnItemTouchListener(new RecyclerItemClickListener(getActivity(), this));
+        tripsRecyclerView.addItemDecoration(new CustomItemDecoration(getActivity(), CustomItemDecoration.VERTICAL_LIST, true));*/
+    }
+
+    private void initAdapter() {
+        tripsRecyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
+        tripsRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new TripOverviewAdapter();
+        tripsRecyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+            handleAdapterOnClickEvent(position);
+
+    }
+
+    private void handleAdapterOnClickEvent(@NonNull final int position) {
+       // FotoNavigation.showStepOverview(getFragmentManager(), caseIdValidationResponseBean.getCaseId(), mAdapter.getIdentificationGroup(position));
     }
 }
