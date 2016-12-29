ed6b85eb81638f90dde257c4a310d0b0a8c17da4
Sarvi <sarvenaz.golchinniksirat@7p-group.com>
2016-12-29 02:20:02 +0100

    added swipe to recyclerView (EDIT is not working properly!)

--- app/src/main/java/com/example/sarvenazgolchinniksirat/costdivider/ui/fragment/TripsOverviewFragment.java
+++ app/src/main/java/com/example/sarvenazgolchinniksirat/costdivider/ui/fragment/TripsOverviewFragment.java
@@ -2,6 +2,12 @@ package com.example.sarvenazgolchinniksirat.costdivider.ui.fragment;
 
 
 import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,6 +15,7 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -40,6 +47,10 @@ public class TripsOverviewFragment extends Fragment {
 
     List<TripOverviewData> tripsNameList = new ArrayList<>();
 
+    private Paint p = new Paint();
+    private int edit_position;
+    private boolean add = false;
+
     @NonNull
     public static TripsOverviewFragment newInstance() {
 
@@ -61,12 +72,12 @@ public class TripsOverviewFragment extends Fragment {
         initRecyclerView();
         initAdapter();
         initDialog();
-       // initSwipe();
+        initSwipe();
 
         return rootView;
     }
 
-   /*private void initSwipe() {
+   private void initSwipe() {
         ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
 
             @Override
@@ -103,7 +114,7 @@ public class TripsOverviewFragment extends Fragment {
                         p.setColor(Color.parseColor("#388E3C"));
                         RectF background = new RectF((float) itemView.getLeft(), (float) itemView.getTop(), dX, (float) itemView.getBottom());
                         c.drawRect(background, p);
-                        icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_edit_black);
+                        icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_edit_white);
                         RectF icon_dest = new RectF((float) itemView.getLeft() + width, (float) itemView.getTop() + width, (float) itemView.getLeft() + 2 * width, (float) itemView.getBottom() - width);
                         c.drawBitmap(icon, null, icon_dest, p);
                     } else {
@@ -120,7 +131,7 @@ public class TripsOverviewFragment extends Fragment {
         };
         ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
         itemTouchHelper.attachToRecyclerView(tripsRecyclerView);
-    }*/
+    }
 
     private void removeView() {
         if (view.getParent() != null) {
@@ -136,9 +147,17 @@ public class TripsOverviewFragment extends Fragment {
         alertDialog.setPositiveButton("Save", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
+                if(add){
+                    add =false;
                     tripOverviewData.setTripName(et_country.getText().toString());
                     mAdapter.addItem(tripOverviewData);
                     dialog.dismiss();
+                } else {
+                    tripOverviewData.setTripName(et_country.getText().toString());
+                    mAdapter.addItem(tripOverviewData);
+                    mAdapter.notifyDataSetChanged();
+                    dialog.dismiss();
+                }
             }
         });
         et_country = (EditText) view.findViewById(R.id.et_country);
@@ -147,6 +166,7 @@ public class TripsOverviewFragment extends Fragment {
     @OnClick(R.id.fab_add)
     protected void addTrip() {
         removeView();
+        add = true;
         alertDialog.setTitle(R.string.trip_name_dialog_title);
         et_country.setText("");
         alertDialog.show();
